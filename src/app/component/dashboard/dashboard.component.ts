d17c5ae6d40520e9946d087d83c4baf15760a77b
BL-Jaspreet <jaspreet.flora@bridgelabz.com>
2023-10-14 14:43:55 +0530

    [Jaspreet]added: Rendered header in dashboard

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  //  encapsulation: ViewEncapsulation.None, // material css chnage
+})
+export class DashboardComponent {}
